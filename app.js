f6c6c14635d4a938736e55d83b9a014665813cfa
mateusMBA <mateus.barros.almeida@gmail.com>
2021-08-29 14:24:08 -0300

    server initialization change

--- app.js
+++ app.js
@@ -26,20 +26,28 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/register', registerRouter);
 app.use('/docs', swaggerUI.serve, swaggerUI.setup(swaggerDocs));
 
+app.get('/', (req,res) =>{
+  res.send('Hello World');
+})
+
+app.listen(process.env.PORT || 3000, () =>{
+  console.log('Server has started')
+})
+
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
+// app.use(function(req, res, next) {
+//   next(createError(404));
+// });
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+// // error handler
+// app.use(function(err, req, res, next) {
+//   // set locals, only providing error in development
+//   res.locals.message = err.message;
+//   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
+//   // render the error page
+//   res.status(err.status || 500);
+//   res.render('error');
+// });
 
 module.exports = app;
